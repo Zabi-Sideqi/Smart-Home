68135825eacd9b86113859a0c9bc49580efdcbd6
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-06-05 12:47:58 +0200

    Del 3 - Appliance basklass

---
+++ Smart-Home/Appliance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Home
+{
+    public class Appliance
+    {
+        public string Brand { get; }
+        public string Room { get; }
+        public bool IsOn { get; protected set; }
+
+        public Appliance(string brand, string room)
+        { 
+            // Sätt Brand, Room och IsOn. 
+            Brand = brand;
+            Room = room;
+            IsOn = false;
+        }
+
+        public virtual string GetInfo()
+        {
+            // Returnera en generell text om apparaten. 
+            // Exempel: "LG in Laundry room" 
+            return $"{Brand} in {Room}";
+            
+        }
+
+        public virtual void TurnOn()
+        {
+           
+            // Sätt IsOn till true. 
+            // Skriv ut ett generellt startmeddelande. 
+            IsOn = true;
+            Console.WriteLine($"{Brand} is now on.");
+            
+        }
+
+        public virtual void TurnOff()
+        {
+           
+            // Sätt IsOn till false. 
+            // Skriv ut ett generellt stoppmeddelande. 
+            IsOn = false;
+            Console.WriteLine($"{Brand} is now off.");
+           
+        }
+
+        public virtual double GetDailyEnergyUsage()
+        {            
+            // Returnera 0 som standardvärde. 
+            return 0;
+            
+        }
+    }
+}
